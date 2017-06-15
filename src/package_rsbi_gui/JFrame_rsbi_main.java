89e62259732481965c5eca83d36f868ce78dc5a2
Janselepoor <jan-felix@hotmail.com>
2017-06-15 21:21:07 +0200

    new commit

--- src/package_rsbi_gui/JFrame_rsbi_main.java
+++ src/package_rsbi_gui/JFrame_rsbi_main.java
@@ -49,6 +49,11 @@ public class JFrame_rsbi_main extends javax.swing.JFrame {
         });
 
         jButton3.setText("logout");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,11 +87,19 @@ public class JFrame_rsbi_main extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         new JFrame_rsbi_search().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        new JFrame_rsbi_addrecipeform().setVisible(true);
+        this.setVisible(false);
+        
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
