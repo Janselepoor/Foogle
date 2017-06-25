28cf80fccc39237f734cb41e8ae09f0df9c1053a
Lapompa <c.pompa@gmx.de>
2017-06-25 12:43:58 +0200

    Patrick ist stramm!
    login & registration funktionieren jetzt auch mit enter

--- src/package_rsbi_gui/JFrame_rsbi_recipetable.java
+++ src/package_rsbi_gui/JFrame_rsbi_recipetable.java
@@ -113,7 +113,7 @@ public class JFrame_rsbi_recipetable extends javax.swing.JFrame {
 
     private void jButton_cancel_recipetableActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_cancel_recipetableActionPerformed
         this.dispose();
-        new JFrame_rsbi_search().setVisible(true);
+        new new_search().setVisible(true);
     }//GEN-LAST:event_jButton_cancel_recipetableActionPerformed
 
     private void jButton_show_recipetableActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_show_recipetableActionPerformed
