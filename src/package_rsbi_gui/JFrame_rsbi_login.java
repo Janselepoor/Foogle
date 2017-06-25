dd6b8572383c993d11b6ef8963ac205d03df54d6
rebecca <rebecca@192.168.0.178>
2017-06-25 18:39:00 +0200

    Merge origin/master
    Conflicts:
    	src/package_rsbi_gui/JFrame_rsbi_addrecipeform.java

--- src/package_rsbi_gui/JFrame_rsbi_login.java
+++ src/package_rsbi_gui/JFrame_rsbi_login.java
@@ -5,6 +5,7 @@
  */
 package package_rsbi_gui;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.sql.Connection;
 import javax.swing.JFrame;
@@ -18,12 +19,16 @@ import methoden_rsbi.Verbindung;
  * @author JF
  */
 public class JFrame_rsbi_login extends javax.swing.JFrame {
+    
+    
 
     /**
      * Creates new form JFrame_rsbi_login
      */
     public JFrame_rsbi_login() {
         initComponents();
+        getContentPane().setBackground(Color.WHITE);
+        
     }
 
     /**
@@ -35,31 +40,22 @@ public class JFrame_rsbi_login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton_registration_login = new javax.swing.JButton();
-        jButton_login_login = new javax.swing.JButton();
         jButton_connect_login = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
         jTextField_username_login = new javax.swing.JTextField();
         jLabel_username_login = new javax.swing.JLabel();
-        jLabel_password_login = new javax.swing.JLabel();
         jPasswordField_login = new javax.swing.JPasswordField();
+        jLabel_password_login = new javax.swing.JLabel();
+        jButton_login_login = new javax.swing.JButton();
+        jButton_registration_login = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("foogle");
+        setBackground(new java.awt.Color(255, 255, 255));
+        setPreferredSize(new java.awt.Dimension(425, 300));
         setResizable(false);
-
-        jButton_registration_login.setText("Registration");
-        jButton_registration_login.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton_registration_loginActionPerformed(evt);
-            }
-        });
-
-        jButton_login_login.setText("Login");
-        jButton_login_login.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton_login_loginActionPerformed(evt);
-            }
-        });
 
         jButton_connect_login.setText("Connect");
         jButton_connect_login.addActionListener(new java.awt.event.ActionListener() {
@@ -68,15 +64,18 @@ public class JFrame_rsbi_login extends javax.swing.JFrame {
             }
         });
 
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setPreferredSize(new java.awt.Dimension(261, 198));
+
         jTextField_username_login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField_username_loginActionPerformed(evt);
             }
         });
 
+        jLabel_username_login.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel_username_login.setForeground(new java.awt.Color(102, 102, 102));
         jLabel_username_login.setText("Username");
-
-        jLabel_password_login.setText("Password");
 
         jPasswordField_login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -89,6 +88,88 @@ public class JFrame_rsbi_login extends javax.swing.JFrame {
             }
         });
 
+        jLabel_password_login.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel_password_login.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel_password_login.setText("Password");
+
+        jButton_login_login.setBackground(new java.awt.Color(255, 255, 255));
+        jButton_login_login.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
+        jButton_login_login.setForeground(new java.awt.Color(102, 102, 102));
+        jButton_login_login.setText("Login");
+        jButton_login_login.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_login_loginActionPerformed(evt);
+            }
+        });
+
+        jButton_registration_login.setBackground(new java.awt.Color(255, 255, 255));
+        jButton_registration_login.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jButton_registration_login.setForeground(new java.awt.Color(102, 102, 102));
+        jButton_registration_login.setText("Register");
+        jButton_registration_login.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_registration_loginActionPerformed(evt);
+            }
+        });
+
+        jTextField1.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        jTextField1.setForeground(new java.awt.Color(255, 0, 0));
+        jTextField1.setText("LOGIN");
+        jTextField1.setToolTipText("");
+        jTextField1.setBorder(null);
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addComponent(jButton_login_login, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton_registration_login, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jPasswordField_login, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel_password_login))))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addComponent(jTextField_username_login, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel_username_login)))
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField_username_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel_username_login))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jPasswordField_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel_password_login))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton_login_login)
+                    .addComponent(jButton_registration_login))
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/package_rsbi_gui/Google-5.png"))); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -99,38 +180,24 @@ public class JFrame_rsbi_login extends javax.swing.JFrame {
                         .addContainerGap()
                         .addComponent(jButton_connect_login))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(86, 86, 86)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField_username_login, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE)
-                                    .addComponent(jPasswordField_login))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel_username_login)
-                                    .addComponent(jLabel_password_login)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton_login_login, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton_registration_login)))))
-                .addContainerGap(126, Short.MAX_VALUE))
+                        .addGap(25, 25, 25)
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(80, 80, 80)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField_username_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel_username_login))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel_password_login)
-                    .addComponent(jPasswordField_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton_registration_login)
-                    .addComponent(jButton_login_login))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 101, Short.MAX_VALUE)
+                .addGap(16, 16, 16)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 52, Short.MAX_VALUE)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(12, 12, 12)))
                 .addComponent(jButton_connect_login)
                 .addContainerGap())
         );
@@ -216,6 +283,10 @@ public class JFrame_rsbi_login extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jPasswordField_loginKeyPressed
 
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -255,9 +326,12 @@ public class JFrame_rsbi_login extends javax.swing.JFrame {
     private javax.swing.JButton jButton_connect_login;
     private javax.swing.JButton jButton_login_login;
     private javax.swing.JButton jButton_registration_login;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel_password_login;
     private javax.swing.JLabel jLabel_username_login;
+    private javax.swing.JPanel jPanel2;
     public static javax.swing.JPasswordField jPasswordField_login;
+    private javax.swing.JTextField jTextField1;
     public static javax.swing.JTextField jTextField_username_login;
     // End of variables declaration//GEN-END:variables
 }
